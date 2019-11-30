b879f3d82ba40a86d485cc09efc69d48f0ea0e69
Jay Lee <jaewon@jaewon.me>
2019-11-30 15:42:42 +0900

    error patch

--- backend/models/user.py
+++ backend/models/user.py
@@ -71,7 +71,7 @@ class Friend(object):
                 "weight": friend_object.weight,
                 "goal_weight": friend_object.goal_weight,
                 "now_weight": friend_object.now_weight,
-                "percentage": (friend_object.weight - friend_object.now_weight) / (friend_object.weight - friend_object.goal_weight) * 100
+                "percentage": (float(friend_object.weight) - float(friend_object.now_weight)) / (float(friend_object.weight) - float(friend_object.goal_weight)) * 100.0 + "%"
             })
 
         return jsonify(response), 200
